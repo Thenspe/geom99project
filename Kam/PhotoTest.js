85f82f1e887237892c5af463f2a1bc58b74dc79b
KamrynK <kamryn.sandiford@flemingcollege.ca>
2023-04-14 00:24:58 -0400

    photo overlay

---
+++ Kam/PhotoTest.js
@@ -0,0 +1,55 @@
+// Add map baselayers
+const osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+});
+const Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+});
+//Map declaration
+const map = L.map('map', {
+    center: [45.000, -78.304],
+    zoom: 8,
+    layers: [osm, Esri_WorldImagery]            
+});
+// Add baselayer info as array for layer control, control added lower to put it
+// below the search bar
+const baseLayers = {
+    'OpenStreetMap': osm,
+    'Esri World Imagery': Esri_WorldImagery
+};
+
+// add geosearch control
+var geocoder = L.Control.geocoder({
+    collapsed: false,
+    position: 'topright',
+    defaultMarkGeocode: false
+}).on('markgeocode', function(result) {
+    const coords = [result.geocode.center.lat, result.geocode.center.lng];
+    var searchMarker = L.marker(coords, {
+        draggable: true //create draggable marker
+    }).addTo(map);
+    map.setView(coords,17);
+})
+.addTo(map);
+
+    //Add layer control button to switch between imagery and openstreetmap
+    const layerControl = L.control.layers(baseLayers).addTo(map);
+
+    // add air photo database geojson
+    /*        
+    var airphotos = L.geoJSON(photos, {
+        onEachFeature: function (feature, info) {
+            info.bindPopup('<p>Photo ID: '+feature.properties.PHOTO_ID+'</p>')
+        }
+    }).addTo(map);
+    */
+    var image1URL = 'images/A16868_154/A16868_154.png',
+        imageErrorOverlayUrl = 'https://cdn-icons-png.flaticon.com/512/110/110686.png',
+        altText = 'alt text'
+        image1Bounds = L.latLngBounds([[44.385133704289053, -78.745641039671753], [44.325644754986556, -78.659455982686808]]);
+
+    var image1overlay = L.imageOverlay(image1URL, image1Bounds, {
+        errorOverlayURL: imageErrorOverlayUrl,
+        alt: altText
+    }).addTo(map);  
