7fc1e922d891fa610100cdea17763113ec72e139
KamrynK <kamryn.sandiford@flemingcollege.ca>
2023-05-19 16:16:48 -0400

    syntax fixes

--- APmap1.js
+++ APmap1.js
@@ -2,7 +2,7 @@ var map = L.map('map',{
 }).setView([45.000, -78.304], 8,);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>', 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     subdomains: 'abcd',
     ext: 'png'
 }).addTo(map);
